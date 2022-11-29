fda54ad99ae7aa51f7feecf3282de4acf3f34ff1
alejandroCinelli <alejandro.cinelli@hotmail.com>
2022-11-29 18:19:21 -0300

    3er

--- src/routes/index.js
+++ src/routes/index.js
@@ -23,9 +23,9 @@ router.get("/products", (req, res) => {
 */
 
 // Render con Pug 
-
+/*
 router.route("/").get((req, res) => {
-    console.log("Estoy en el get");
+    //console.log("Estoy en el get");
     res.render("productForm.pug");
 });
 
@@ -37,8 +37,28 @@ router.post("/products", (req, res) => {
 });
 
 router.route("/products").get((req, res) => {
-    console.log("Estoy en el get del products");
+    //console.log("Estoy en el get del products");
     res.render("products.pug", {products});
+});
+*/
+
+// Render con Handlebars 
+
+router.route("/").get((req, res) => {
+    //console.log("Estoy en el get productsForms");
+    res.render("productForm.hbs");
+ });  
+
+router.post("/product", (req, res) => {
+    const { name, price, thumbnail } = req.body;
+    products.push({ name, price, thumbnail });
+    console.log(products);
+    res.redirect("/");
+});
+
+router.route("/products").get((req, res) => {
+    //console.log("Estoy en el get del products");
+    res.render("products.hbs", {products});
 });
 
 export default router;
--- src/server.js
+++ src/server.js
@@ -2,7 +2,8 @@ import express from "express";
 import { json, urlencoded } from 'express';
 import path from "path";
 import { fileURLToPath } from "url";
-import { dirname } from "path";
+import { dirname, join } from "path";
+import { engine } from "express-handlebars";
 import routes from "./routes/index.js";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -20,8 +21,24 @@ app.set("views", __dirname + "/views");
 
 // seteamos para que use pug como motor de plantillas
 
-app.set("view engine", "pug");
+/* app.set("view engine", "pug");
 app.set("views", __dirname + "/views.pug");
+*/
+
+// seteamos para que se use Handlebars como motor de plantillas 
+
+app.engine(
+  "hbs",
+  engine({
+    extname: ".hbs",
+    defaultLayout: join(__dirname, "./views.hbs/layouts/main.hbs"),
+    layoutsDir: join(__dirname, "./views.hbs/layouts"),
+    partialsDir: join(__dirname, "./views.hbs/partials"),
+  })
+);
+
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views.hbs");
 
 app.use("/", routes);
 
